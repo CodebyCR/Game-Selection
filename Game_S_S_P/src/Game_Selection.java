51cfcba87c711d48d0a39e9f9e9f024d98382b43
CR <44606665+CodebyCR@users.noreply.github.com>
2022-06-24 20:20:09 +0200

    Translate the game Language in english and more refactoring.

--- Game_S_S_P/src/Game_Selection.java
+++ Game_S_S_P/src/Game_Selection.java
@@ -1,23 +1,29 @@
 /********************************************************************************************************
-*																										*
-*							Christoph Rohde - Schere, Stein, Papier Class								*
-*																										*
-*									All rights reserved - Version 1.0									*
-*																										*
-*									Fehlermeldung bei keinern Eingabe!									*																
-*																										*
-********************************************************************************************************/
+ *																										*
+ *	Game_Selection by Christoph Rohde 																	*
+ *																										*
+ *	- Version 2.0																						*
+ *																										*
+ *	feel free to modify, and have fun to code.							 								*
+ *																										*
+ ********************************************************************************************************/
 import javax.swing.JOptionPane;
 
+
 public class Game_Selection {		//Schere, Stein, Papier.
-	
+
+private static final int ROCK = 1;
+private static final int PAPER = 2;
+private static final int SCISSORS = 3;
+
 	public static void game_menu_ssp() {
 		String blend_layout;
-		int choise_blend = RandomGame.rendomNumber(1, 3);
+		int playmodi = RandomGame.rendomNumber(1, 3);
 
-		//This is for a random Playmodus, for more Variation.
-		switch(choise_blend) {
-			//enemy
+		//This is for a random playmodus, for more variation.
+		//refactor to map
+		switch(playmodi) {
+				//enemy
 			case 1: blend_layout = "W�hle deine Waffe: ";
 				break;
 				//classic 1
@@ -35,50 +41,55 @@ public class Game_Selection {		//Schere, Stein, Papier.
 		
 	}
 
-	public static void play_ssp (int my_choise) {	
-		String game_out = "";
-		int com_choise = RandomGame.rendomNumber(1, 3);
-		
-		if (my_choise == com_choise ){
-			game_out = "Ha! Unentschieden :D";
-		}
-		else if (my_choise == 1 & com_choise == 3){
-			game_out = "Du hast Gewonnen, ich hatte das Papier. :( ";
-		}
-		else if (my_choise == 2 & com_choise == 1){
-			game_out = "Du hast Gewonnen, ich hatte die Schere. :( ";
-		}
-		else if (my_choise == 3 & com_choise == 2){
-			game_out = "Du hast Gewonnen, ich hatte den Stein. :( ";
-		}
-		else if (my_choise == 1 & com_choise == 2){
-			game_out = "Ich hab Gewonnen, ich hatte den Stein. :) ";
-		}
-		else if (my_choise == 2 & com_choise == 3){
-			game_out = "Ich hab Gewonnen, ich hatte das Papier. :) ";
-		}
-		else if (my_choise == 3 & com_choise == 1){
-			game_out = "Ich hab Gewonnen, ich hatte die Schere. :) ";
-		}
-		else {//if(my_choise != 1 | my_choise != 2| my_choise != 3){
-			game_out = "Hay, nicht schummeln. :P ";
-		}
-		
-		JOptionPane.showMessageDialog(null, game_out);
+	public static void play_ssp (int myChoise) {
 
-		String roundMessage = "Noch eine Runde? ;) \n1)\tJa\n2)\tNein";
-		String runde = JOptionPane.showInputDialog(null,roundMessage);
-		int runde_int = Integer.parseInt(runde);
+		int comChoise = RandomGame.rendomNumber(1, 3);
+
+
+		String result = calculateResult(myChoise, comChoise);
+
 		
-		if (runde_int == 1) {
+		JOptionPane.showMessageDialog(null, result);
+
+		String roundMessage = "Next round? ;) \n1)\tYes\n2)\tNo";
+		String wantNextRound = JOptionPane.showInputDialog(null,roundMessage);
+		int nextRound = wantNextRound.compareToIgnoreCase("Yes");
+		
+		if (nextRound == 1) {
 			game_menu_ssp();
 		}
 		else {
-			JOptionPane.showMessageDialog(null, "Bis zum n�chsten mal. :) ");	
+			JOptionPane.showMessageDialog(null, "See you soon. :) ");
 		}
 	}
 
-	
+	private static String calculateResult(int myChoise, int comChoise) {
+
+		if (myChoise == comChoise) {
+			return "Ha! Unentschieden :D";
+		}
+
+		boolean YouWin = myChoise == ROCK & comChoise == SCISSORS
+						|| myChoise == PAPER & comChoise == ROCK
+						|| myChoise == SCISSORS & comChoise == PAPER;
+
+		//Youlose
+		//		|| myChoise == 1 & comChoise == 2
+		//		|| myChoise == 2 & comChoise == 3
+		//		|| myChoise == 3 & comChoise == 1;
+
+		if(YouWin){
+			//comChoise to String
+			return "You win!!!/n Your choise was " + myChoise + " and the computer choose " +  comChoise +  ". :)";
+		}
+		else{
+			return "You Lose :(/n Your choise was " + myChoise + " and the computer choose " +  comChoise +  ".";
+		}
+
+
+	}
+
+
 	public static void main(String[] args) {
 		game_menu_ssp();
 	}
